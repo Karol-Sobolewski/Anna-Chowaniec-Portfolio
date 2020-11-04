b6fc9e8924bf2eefad161871b82ccfc965109b27
Karol <karolsobolewski92@gmail.com>
2020-11-04 16:44:37 +0100

    Add basic menu and logo

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import styles from './Header.module.scss';
+import styles from './Header.module.scss'; //eslint-disable-line
 
 class Header extends React.Component {
   render() {
     return (
       <header className={styles.component}>
         <nav>
-          <NavLink to="/" activeClassName="active">
-            Home
-          </NavLink>
+          <Link to="/" className={styles.logo}>
+            <img
+              type="image/svg+xml"
+              src="/images/logo/logo-black.svg"
+              className={styles.logoImg}
+              aria-label="Logo"
+            />
+          </Link>
           <NavLink to="/wedding" activeClassName="active">
             Fotografia ślubna
+          </NavLink>
+          <NavLink to="/children" activeClassName="active">
+            Fotografia dziecięca
+          </NavLink>
+          <NavLink to="/portrait" activeClassName="active">
+            Fotografia portretowa
           </NavLink>
         </nav>
       </header>
