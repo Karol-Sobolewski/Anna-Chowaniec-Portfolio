7d010a1aa3b11f746814662637c954ed7587be78
Karol <karolsobolewski92@gmail.com>
2020-11-12 17:22:30 +0100

    Fix styling for Header and Home components

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 import styles from './Header.module.scss'; //eslint-disable-line
 const Header = () => {
   const Menu = useSelector((state) => state.Menu);
-
   return (
     <header className={styles.component}>
       <nav>
