c0050b87eec979fd30ec4813a5a6568789887fd6
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-04-20 20:28:21 +0200

    Remove Redirect in NotFound.js

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,38 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { Redirect } from 'react-router';
 
 import { Container, Row, Col } from 'react-bootstrap';
 import styles from './NotFound.module.scss';
 
-const Component = ({ className, children }) => {
-  const [redirect, setRedirect] = useState(false);
-  useEffect(() => {
-    const timeOut = setTimeout(() => setRedirect(true), 5000);
-    return () => clearTimeout(timeOut);
-  });
-  return (
-    <div className={clsx(className, styles.root)}>
-      {redirect ? (
-        <Redirect to="/" />
-      ) : (
-        <Container>
-          <Row>
-            <Col>
-              <div>
-                <h2>404 - Nieznana strona</h2>
-                <p>Za 5 sekund nastąpi przekierowanie stronę główną</p>
-                <a href="/">Kliknij aby przejśc od razu</a>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      )}
-      <main>{children}</main>
-    </div>
-  );
-};
+const Component = ({ className, children }) => (
+  <div className={clsx(className, styles.root)}>
+    <Container>
+      <Row>
+        <Col>
+          <div>
+            <h2>404 - Nieznana strona</h2>
+            <a href="/">Kliknij aby przejśc na stronę główną</a>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+    <main>{children}</main>
+  </div>
+);
 
 Component.propTypes = {
   children: PropTypes.node,
