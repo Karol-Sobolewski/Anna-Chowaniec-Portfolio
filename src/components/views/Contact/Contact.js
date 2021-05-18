952693c76eca16371160ae072e44344012255e0c
Karol <karolsobolewski92@gmail.com>
2021-05-18 10:26:50 +0200

    Fix stying for social media textareas

--- src/components/views/Contact/Contact.js
+++ src/components/views/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useAuth0 } from "@auth0/auth0-react"; //eslint-disable-line
+import { useAuth0 } from '@auth0/auth0-react';
 
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
@@ -10,13 +10,11 @@ import autosize from 'autosize';
 
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faIcons, fas } from '@fortawesome/free-solid-svg-icons';
-import { IconsGenerator } from '../../common/IconsGenerator/IconsGenerator';
-import { Button } from '../../common/Button/Button';
-
 import styles from './Contact.module.scss';
 
+import { Button } from '../../common/Button/Button';
+import { IconsGenerator } from '../../common/IconsGenerator/IconsGenerator';
 import { editDescriptionRequest } from '../../../redux/descriptionRedux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 const Component = ({ className, children }) => {
   const allPages = useSelector((state) => state.descriptions.data);
@@ -94,7 +92,11 @@ const Component = ({ className, children }) => {
               )
             ) : null
           )}
-          <Row className={styles.socialIconsRow}>
+          <Row
+            className={
+              edit ? styles.socialIconsRow__edit : styles.socialIconsRow
+            }
+          >
             {contactPage.description.map((item) =>
               // eslint-disable-next-line no-nested-ternary
               item.type === `web` ? (
@@ -137,52 +139,6 @@ const Component = ({ className, children }) => {
           />
         ) : null}
       </Row>
-      {/* {contactPage.description.map((item) => (
-        <form
-          action="#"
-          method="put"
-          // onSubmit={}
-          onChange={(e) => handleChange(e, item)}
-          className={styles.editContact}
-          key={contactPage.description.indexOf(item)}
-        >
-          <Row className="w-100 py-3 justify-content-center">
-            <Col className="col-12 col-md-6">
-              {isAuthenticated && edit ? (
-                <div>
-                  <p>Tytuł</p>
-                  <input
-                    name="heading"
-                    type="text"
-                    defaultValue={item.heading}
-                  />
-                </div>
-              ) : null}
-            </Col>
-            <Col className={`col-12 col-md-6 ${edit ? `d-none` : `flex`}`}>
-                {item.type === `mail` ? (
-                  <a href={`mailto:${item.value}`}>{item.text}</a>
-                ) : item.type === `phone` ? (
-                  <a href={`tel:${item.value}`}>{item.text}</a>
-                ) : item.type === `web` ? (
-                  <a target="_blank" href={`${item.value}`}>{item.text}</a>
-                ) : null}
-            </Col>
-            <Row className={`${!edit ? `d-none` : `flex`}`}>
-              <Col className="col-12 col-md-6">
-                <p>Tekst</p>
-                <input name="text" type="text" defaultValue={item.text} />
-              </Col>
-              <Col className="col-12 col-md-6">
-                <p>Wartość / Link</p>
-                <input name="text" type="value" defaultValue={item.value} />
-              </Col>
-            </Row>
-          </Row>
-        </form>
-      ))}
-      */}
-
       <main>{children}</main>
     </div>
   );
