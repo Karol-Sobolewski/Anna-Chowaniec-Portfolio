0c000f28835bd889e251bd28781003405c8dc218
Karol <karolsobolewski92@gmail.com>
2021-06-01 18:45:11 +0200

    Add image optimizer while creating category && Fix styling for addCategoryForm & Add page reaload after creation of new category

--- src/components/features/AddCategoryForm/AddCategoryForm.js
+++ src/components/features/AddCategoryForm/AddCategoryForm.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-// import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { useAuth0 } from '@auth0/auth0-react';
+import Resizer from 'react-image-file-resizer';
 
 import ImageUploader from 'react-images-upload';
 import styles from './AddCategoryForm.module.scss';
 import { Button } from '../../common/Button/Button';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 import { addCategoryRequest } from '../../../redux/categoryRedux';
 import { addMenuRequest } from '../../../redux/menuRedux';
 
 const Component = ({ className }) => {
+  const allMenus = useSelector((state) => state.menu.data);
+  const filtered = allMenus.filter((item) => item.component === `GalleryPage`);
+
   const dispatch = useDispatch();
   const { getAccessTokenSilently } = useAuth0();
   const [category, setCategory] = useState({
@@ -20,12 +22,28 @@ const Component = ({ className }) => {
     description: ``,
     component: `GalleryPage`,
     shortName: ``,
-    order: 3, // TODO get last order of GalleryPage
+    order: filtered.length + 1,
     photo: {
       alt: ``,
       file: null,
     },
   });
+
+  const resizeFile = (file) =>
+    new Promise((resolve) => {
+      Resizer.imageFileResizer(
+        file,
+        1024,
+        1024,
+        `WEBP`,
+        90,
+        0,
+        (uri) => {
+          resolve(uri);
+        },
+        `file`
+      );
+    });
 
   const handleChange = (e) => {
     const { target } = e;
@@ -39,29 +57,24 @@ const Component = ({ className }) => {
       ...category,
       shortName: category.name,
     });
-  }, [category.name]); //eslint-disable-line
+  }, [category.name]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     setCategory({
       ...category,
       photo: { ...category.photo, alt: category.description },
     });
-  }, [category.description]); //eslint-disable-line
+  }, [category.description]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  // useEffect(() => {
-  //   setCategory({
-  //     ...category,
-  //     photo: { alt: category.description },
-  //   });
-  // }, [category.photo]); //eslint-disable-line
-
-  const handleImage = (files) => {
-    if (files)
+  const handleImage = async (files) => {
+    if (files[0]) {
+      const image = await resizeFile(files[0]);
       setCategory({
         ...category,
-        photo: { ...category.photo, file: files[0] },
+        photo: { ...category.photo, file: image },
       });
-    else setCategory({ ...category, photo: { ...category.photo, file: null } });
+    } else
+      setCategory({ ...category, photo: { ...category.photo, file: null } });
   };
 
   const handleSubmit = async (e) => {
@@ -75,6 +88,7 @@ const Component = ({ className }) => {
     categoryFormData.append(`file`, category.photo.file);
     dispatch(addCategoryRequest(categoryFormData, token));
     dispatch(addMenuRequest(category, token));
+    window.location.reload(false);
   };
   return (
     <form
