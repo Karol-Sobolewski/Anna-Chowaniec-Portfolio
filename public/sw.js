fb40b4a1c247c7518a1e3bea0f504332977aaf27
Karol <karolsobolewski92@gmail.com>
2021-08-09 12:40:22 +0200

    Add manifest.json

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const cacheName = `app-name`;
+const filesToCache = [`/images/logo.svg`, `/index.html`];
+self.addEventListener(`install`, function (e) {  //eslint-disable-line
+  console.log(`[ServiceWorker] Install`);
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log(`[ServiceWorker] Caching app shell`);
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener(`activate`, (event) => {//eslint-disable-line
+  event.waitUntil(self.clients.claim());//eslint-disable-line
+});
+self.addEventListener(`fetch`, (event) => {//eslint-disable-line
+  event.respondWith(
+    caches
+      .match(event.request, { ignoreSearch: true })
+      .then((response) => response || fetch(event.request))
+  );
+});
